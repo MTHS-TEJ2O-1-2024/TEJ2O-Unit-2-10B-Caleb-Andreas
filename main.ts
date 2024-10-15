27c290588242f6731776f05236f8ab8c2ddf6f1f
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-15 16:52:48 -0400

    Made light meter code

--- main.ts
+++ main.ts
@@ -2,7 +2,83 @@
  *
  * Created by: Caleb Andreas
  * Created on: Oct 2024
- * This program is a light meter.
+ * This program is a light meter that turns on neopixels 
+ * depending on how bright it is.
 */
 
-basic.showString('Hello, World!')
+// Variables.
+let lightLevel: number
+let neopixelStrip: neopixel.Strip = null
+
+// Cleanup then happy face at start
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// Start code on button A pressed.
+input.onButtonPressed(Button.A, function () {
+    // Turn off all LEDs and neopixels at start.
+    basic.clearScreen()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.pause(500)
+
+    // Get light level then back to happy.
+    lightLevel = input.lightLevel()
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+
+    // Set all neopixels to black if lightLevel <= 51.
+    if (lightLevel <= 51) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+    // Neopixel 0 set to green if lightLevel > 52.
+    if (lightLevel > 52) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+    // Neopixel 1 set to yellow if lightLevel > 104.
+    if (lightLevel > 104) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+    // Neopixel 2 set to orange if lightLevel > 156.
+    if (lightLevel > 156) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+
+    // Neopixel 3 set to red if lightLevel > 208.
+    if (lightLevel > 208) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+    }
+
+})
