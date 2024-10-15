2d4d99ef61d8e0458a405e3d88609c2fa2910ec0
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-15 11:24:04 -0400

    started program

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Oct 2024
+ * This program is a light meter.
 */
 
 basic.showString('Hello, World!')
